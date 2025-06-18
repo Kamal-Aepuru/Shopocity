d87b68a7f3bbd99c6010f2432c4fa9c82c66c541
Kamal <kamalaepuru@gmail.com>
2025-06-18 14:19:45 +0530

    update:changed the product details logic

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,21 +1,21 @@
-// import useProducts from "@/api/useProducts";
-// import { useParams } from "@tanstack/react-router";
+import useProducts from "@/api/useProducts";
+import { useParams } from "@tanstack/react-router";
 const ProductDetails = () => {
-//  const { data: _products, isLoading } = useProducts();
-// const {productId} = useParams({from:'/products/$productId'});
-// console.log(productId)
-// if (isLoading) return <p>Loading...</p>;
-return(<div>Hey Inside Product Details</div>)
-// const product = products?.find((p) => p._id === productId);
-// if (!product) return <p>Product not found</p>;
+  const { data: products, isLoading } = useProducts();
+  const { productId } = useParams({
+    from: "/products/productDetails/$productId",
+  });
+  console.log(productId);
+  if (isLoading) return <p>Loading...</p>;
+  const product = products?.find((p) => p._id === productId);
+  if (!product) return <p>Product not found</p>;
 
-// return (
-//   <div className="p-6">
-//     <h1>{product.name}</h1>
-//     <p>{product.description}</p>
-//   </div>
-// );
-
-}
+  return (
+    <div className="p-6">
+      <h1>{product?.name}</h1>
+      <p>{product?.description}</p>
+    </div>
+  );
+};
 
 export default ProductDetails;
